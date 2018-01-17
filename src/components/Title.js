1b556950152a5c96c22ddc780aee40c30d28fe8c
MirekKopec <mkopec95@gmail.com>
2018-01-17 22:40:22 +0100

    project first commit

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = props => 
+
+    <div>
+        <h1>{props.title}</h1>
+        <p>Number of tasks: {props.count}</p>
+    </div>
+
+
+export default Title;
